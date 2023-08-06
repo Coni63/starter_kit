4e84f7e7e97e2eb611a96e8de53e4de614ea5cfe
nicolas <mine.nicolas@gmail.com>
2023-08-06 13:21:01 +0200

    add sample codes for applications

--- src/api/module1/routes.py
+++ src/api/module1/routes.py
@@ -1,9 +1,10 @@
 from __future__ import annotations
 
+from app.module1.application.use_cases import basic_application
+from app.module1.domain.item import Item
 from fastapi import APIRouter
 from fastapi import HTTPException
 from fastapi import status
-
 
 url_prefix = "module1"
 openapi_tag = {
@@ -23,50 +24,48 @@ router = APIRouter(
 
 
 @router.get("/", tags=[openapi_tag["name"]])
-async def get_all_items() -> list[dict]:
-    all_objects = [{"n": i} for i in range(10)]
-    return all_objects
+def get_all_items() -> list[dict]:
+    return basic_application.get_all_items()
 
 
 @router.get("/{id}", tags=[openapi_tag["name"]])
-async def get_item_by_id(id: int) -> dict:
-    return {"n": id}
+def get_item_by_id(id: int) -> dict:
+    return basic_application.get_item_by_id(id)
 
 
 @router.post("/create/", tags=[openapi_tag["name"]], status_code=status.HTTP_201_CREATED)
-async def read_item(new_item: int) -> dict:
-    valid = new_item % 2 == 0
-    if valid:
-        return {"n": new_item}
-    raise HTTPException(
-        status_code=status.HTTP_400_BAD_REQUEST,
-        detail="ERROR",
-    )
+def create_item(item: Item) -> dict:
+    try:
+        return basic_application.create_item(item)
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=str(e),
+        )
 
 
 @router.put("/update/{id}", tags=[openapi_tag["name"]], status_code=status.HTTP_200_OK)
-async def update_item(id: int, new_item: int) -> dict:
-    valid = new_item % 2 == 0
-    if valid:
-        return {"n": new_item}
-    raise HTTPException(
-        status_code=status.HTTP_400_BAD_REQUEST,
-        detail="ERROR",
-    )
+def update_item(id: int, new_item: Item) -> dict:
+    try:
+        return basic_application.update_item(id, new_item)
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=str(e),
+        )
 
 
 @router.patch("/update/{id}", tags=[openapi_tag["name"]], status_code=status.HTTP_200_OK)
-async def patch_item(id: int, new_item: int) -> dict:
-    valid = new_item % 2 == 0
-    if valid:
-        return {"n": new_item}
-    raise HTTPException(
-        status_code=status.HTTP_400_BAD_REQUEST,
-        detail="ERROR",
-    )
+def patch_item(id: int, new_item: Item) -> dict:
+    try:
+        return basic_application.patch_item(id, new_item)
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=str(e),
+        )
 
 
 @router.delete("/detele/{id}", tags=[openapi_tag["name"]], status_code=status.HTTP_200_OK)
-async def delete_item(id: int, new_item: int) -> bool:
-    new_item += 1
-    return True
+def delete_item(id: int) -> bool:
+    return basic_application.delete_item(id)
